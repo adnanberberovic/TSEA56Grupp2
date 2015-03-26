ea215f1c99e9ada56d42b79abff58ea78409bf9e
Måns Skytt <mansk700@studet.liu.se>
2015-03-26 10:20:42 +0100

    Komm ändring

--- DOKUMENT/KONSTRUKTION/KOD/Komm_buss/Komm_buss/Komm_buss.c
+++ DOKUMENT/KONSTRUKTION/KOD/Komm_buss/Komm_buss/Komm_buss.c
@@ -28,7 +28,7 @@ void SPI_SlaveInit(void)
 {
 	SPSR = 0<<SPI2X;
 	SPCR = 1<<SPIE | 1<<SPE | 1<<DORD | 0<<MSTR | 0<<CPOL | 0<<CPHA | 
-1<<SPR1 | 1<<SPR0;
+	1<<SPR1 | 1<<SPR0;
 	//SPIE: SPI interrupt enable. Set to 1 to allow interrupts
 	//SPE: SPI Enable. Set to 1 to allow SPI communication
 	//DORD: Data order. Set to 1 to transmit LSB first, MSB last.
@@ -36,7 +36,7 @@ void SPI_SlaveInit(void)
 	//CPOL and CPHA: set to 0 to sample on rising edge and setup on falling edge.
 	//SPI2X, SPR1, SPR0, set to 0,1,1 to scale clock with f_osc/128.
 	//Bus config similar to comm and sensor module, though set 0<<MSTR
-	
+	sei();
 }
 
 
@@ -46,14 +46,12 @@ void SPI_SlaveTransmit(unsigned char cData)
 	inSPDR = SPDR;
 	//Load data into SPI data register
 	SPDR = cData; 
-	
-
 }
 
 //Interrupt method runs when SPI transmission/reception is completed.
 ISR(SPI_STC_vect)
 {
-	SPI_SlaveTransmit(outSPDR);
+	inSPDR = SPDR;
 	
 }
 
@@ -61,7 +59,6 @@ int main(void)
 {
 	outSPDR = 0x01;
 	inSPDR = 0x00;
-	sei();
 	sleep_enable();
 	Komm_InitPortDirections();
 	Komm_InitPortValues();
