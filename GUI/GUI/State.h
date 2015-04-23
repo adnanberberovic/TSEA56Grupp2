068ffbd3b85c44306dbd1520fbd93efc8b0ac10b
Måns Skytt <mansk700@studet.liu.se>
2015-04-23 21:17:59 +0200

    BT fix

--- GUI/GUI/State.h
+++ GUI/GUI/State.h
@@ -9,22 +9,18 @@
 #ifndef __GUI__State__
 #define __GUI__State__
 
-
 #include <string>
 
 class State
-{
-    
+{    
 public:
-    virtual void      event(std::string& statestring, bool& running) = 0;
-    virtual void      update(std::string& statestring, bool& running) = 0;
-    virtual void      render() = 0;
-    virtual void      run(SetupSDL* sdl_lib __attribute__((unused))
-                          , std::string& statestring) = 0;
-    
+	virtual void      event(std::string& statestring, bool& running) = 0;
+	virtual void      update(std::string& statestring, bool& running) = 0;
+	virtual void      render() = 0;
+	virtual void      run(std::string& statestring) = 0;
 };
 
 
-
+//
 #endif /* defined(__GUI__State__) */
 
