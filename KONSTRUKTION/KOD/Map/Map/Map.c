17c987620f3fd93e2c43a22ac0bd591350d39f83
robop <robop806@student.liu.se>
2015-05-03 21:08:12 +0200

    kartläggning redo att testas!!! (typ)

--- KONSTRUKTION/KOD/Map/Map/Map.c
+++ KONSTRUKTION/KOD/Map/Map/Map.c
@@ -10,6 +10,14 @@
 #include <stdlib.h>
 #include "Map.h"
 
+// FLAGz man!
+unsigned int operatingMode_ = 0; // 0 = normal, 3 = return to prev sq, 4 = resQ
+unsigned int rotating_ = 0;
+unsigned int movingForward_ = 0;
+// unsigned int movingToPrevSq_;
+// unsigned int resQActive_;
+ int LOOPer = 1;
+
 // Sets the position of the goal in the map
 void MAP_setGoal()
 {
@@ -35,10 +43,10 @@ void MAP_setSquareDescription(int description, int posY, int posX)
 	}
 }
 
-// Check if the square is an unexplored road and sets the next direction to face it
-int MAP_checkDirDown()
+// Check if the square is an unexplored road and sets the next direction to face it, send with 0 if exploring
+int MAP_checkDirDown(int exploring)
 {
-	if (MAP_array[MAP_currentPos[0] + 1][MAP_currentPos[1]].description == 3 && !MAP_array[MAP_currentPos[0] + 1][MAP_currentPos[1]].visited)
+	if (MAP_array[MAP_currentPos[0] + 1][MAP_currentPos[1]].description == 3 && (!MAP_array[MAP_currentPos[0] + 1][MAP_currentPos[1]].visited || exploring))
 	{
 		MAP_nextDir = 3;
 		return 1;
@@ -49,10 +57,10 @@ int MAP_checkDirDown()
 	}
 }
 
-// Check if the square is an unexplored road and sets the next direction to face it
-int MAP_checkDirUp()
+// Check if the square is an unexplored road and sets the next direction to face it, send with 0 if exploring
+int MAP_checkDirUp(int exploring)
 {
-	if (MAP_array[MAP_currentPos[0] - 1][MAP_currentPos[1]].description == 3 && !MAP_array[MAP_currentPos[0] - 1][MAP_currentPos[1]].visited)
+	if (MAP_array[MAP_currentPos[0] - 1][MAP_currentPos[1]].description == 3 && (!MAP_array[MAP_currentPos[0] - 1][MAP_currentPos[1]].visited || exploring))
 	{
 		MAP_nextDir = 1;
 		return 1;
@@ -63,10 +71,10 @@ int MAP_checkDirUp()
 	}
 }
 
-// Check if the square is an unexplored road and sets the next direction to face it
-int MAP_checkDirLeft()
+// Check if the square is an unexplored road and sets the next direction to face it, send with 0 if exploring
+int MAP_checkDirLeft(int exploring)
 {
-	if (MAP_array[MAP_currentPos[0]][MAP_currentPos[1] - 1].description == 3 && !MAP_array[MAP_currentPos[0]][MAP_currentPos[1] - 1].visited)
+	if (MAP_array[MAP_currentPos[0]][MAP_currentPos[1] - 1].description == 3 && (!MAP_array[MAP_currentPos[0]][MAP_currentPos[1] - 1].visited || exploring))
 	{
 		MAP_nextDir = 2;
 		return 1;
@@ -77,10 +85,10 @@ int MAP_checkDirLeft()
 	}
 }
 
-// Check if the square is an unexplored road and sets the next direction to face it
-int MAP_checkDirRight()
+// Check if the square is an unexplored road and sets the next direction to face it, send with 0 if exploring
+int MAP_checkDirRight(int exploring)
 {
-	if (MAP_array[MAP_currentPos[0]][MAP_currentPos[1] + 1].description == 3 && !MAP_array[MAP_currentPos[0] + 1][MAP_currentPos[1] + 1].visited)
+	if (MAP_array[MAP_currentPos[0]][MAP_currentPos[1] + 1].description == 3 && (!MAP_array[MAP_currentPos[0] + 1][MAP_currentPos[1] + 1].visited || exploring))
 	{
 		MAP_nextDir = 0;
 		return 1;
@@ -88,6 +96,27 @@ int MAP_checkDirRight()
 	else
 	{
 		return 0;
+	}
+}
+
+// Chechks given direction
+int MAP_checkDir(int dir)
+{
+	if (dir == 0)
+	{
+		return MAP_checkDirRight(1);
+	}
+	else if (dir == 1)
+	{
+		return MAP_checkDirUp(1);
+	}
+	else if (dir == 2)
+	{
+		return MAP_checkDirLeft(1);
+	}
+	else if (dir == 3)
+	{
+		return MAP_checkDirDown(1);
 	}
 }
 
@@ -116,25 +145,25 @@ void MAP_decideDirection(char mode)
 	if (mode_ == 'l')
 	{
 		// Left-first
-		if ( robotDir == 2 && (MAP_checkDirDown() || MAP_checkDirLeft() || MAP_checkDirUp() || MAP_checkDirRight()) )
+		if ( robotDir == 2 && (MAP_checkDirDown(0) || MAP_checkDirLeft(0) || MAP_checkDirUp(0) || MAP_checkDirRight(0)) )
 		{} 
-		else if ( robotDir == 1 && (MAP_checkDirLeft() || MAP_checkDirUp() || MAP_checkDirRight() || MAP_checkDirDown()) )
+		else if ( robotDir == 1 && (MAP_checkDirLeft(0) || MAP_checkDirUp(0) || MAP_checkDirRight(0) || MAP_checkDirDown(0)) )
 		{}
-		else if (  robotDir == 0 && (MAP_checkDirUp() || MAP_checkDirRight() || MAP_checkDirDown() || MAP_checkDirLeft()) )
+		else if (  robotDir == 0 && (MAP_checkDirUp(0) || MAP_checkDirRight(0) || MAP_checkDirDown(0) || MAP_checkDirLeft(0)) )
 		{}
-		else if (  robotDir == 3 && (MAP_checkDirRight() || MAP_checkDirDown() || MAP_checkDirLeft() || MAP_checkDirUp()) )
+		else if (  robotDir == 3 && (MAP_checkDirRight(0) || MAP_checkDirDown(0) || MAP_checkDirLeft(0) || MAP_checkDirUp(0)) )
 		{}
 	}
 	else if (mode_ == 'r')	
 	{
 		// Right-first
-		if (  robotDir == 2 && (MAP_checkDirUp() || MAP_checkDirLeft() || MAP_checkDirDown() || MAP_checkDirRight()) )
+		if (  robotDir == 2 && (MAP_checkDirUp(0) || MAP_checkDirLeft(0) || MAP_checkDirDown(0) || MAP_checkDirRight(0)) )
 		{}
-		else if (  robotDir == 1 && (MAP_checkDirRight() ||  MAP_checkDirUp() || MAP_checkDirLeft() || MAP_checkDirDown()) )
+		else if (  robotDir == 1 && (MAP_checkDirRight(0) ||  MAP_checkDirUp(0) || MAP_checkDirLeft(0) || MAP_checkDirDown(0)) )
 		{}
-		else if (  robotDir == 0 && (MAP_checkDirDown() || MAP_checkDirRight() ||  MAP_checkDirUp() || MAP_checkDirLeft()) )
+		else if (  robotDir == 0 && (MAP_checkDirDown(0) || MAP_checkDirRight(0) ||  MAP_checkDirUp(0) || MAP_checkDirLeft(0)) )
 		{}
-		else if ( robotDir == 3 && (MAP_checkDirLeft() || MAP_checkDirDown() || MAP_checkDirRight() ||  MAP_checkDirUp()) )
+		else if ( robotDir == 3 && (MAP_checkDirLeft(0) || MAP_checkDirDown(0) || MAP_checkDirRight(0) ||  MAP_checkDirUp(0)) )
 		{}
 	}
 }
@@ -149,14 +178,24 @@ void MAP_setVisited()
 void MAP_countUnexploredSquares()
 {
 	int dir_ = MAP_currentDir; //WHY?
-	MAP_unexploredSquares = MAP_checkDirDown() + MAP_checkDirLeft() + MAP_checkDirRight() + MAP_checkDirUp();
+	MAP_unexploredSquares = MAP_checkDirDown(0) + MAP_checkDirLeft(0) + MAP_checkDirRight(0) + MAP_checkDirUp(0);
 	MAP_currentDir = dir_; // WHY?
 }
 
 // Uses Dijkstras algorithm to decide which location to set as a destination
 void MAP_decideDestination()
 {
-	
+	// Simpleton variant
+	MAP_nextJunctionLong = MAP_lastUnexJunction();
+	if (MAP_nextJunctionLong == 420) {}
+	else if (MAP_nextJunctionLong < MAP_currentJunction)
+	{
+		MAP_nextJunctionShort = MAP_currentJunction - 1;
+	}
+	else if (MAP_nextJunctionLong > MAP_currentJunction)
+	{
+		MAP_nextJunctionShort = MAP_currentJunction + 1;
+	}
 }
 
 // Adds the travelled distance from the last junction to specified junction
@@ -172,20 +211,44 @@ void MAP_addJunctionDist(int junction)
 	}
 }
 
+// Adds the direction between two junctions, assuming j1 is the last visited and j2 the current one
+void MAP_addJunctionDir(int j1, int j2, int direction)
+{
+	if (direction == 0)
+	{
+		MAP_junctionOrderArray[j1].right = j2;
+	}
+	else if (direction == 1)
+	{
+		MAP_junctionOrderArray[j1].up = j2;
+	}
+	else if (direction == 2)
+	{
+		MAP_junctionOrderArray[j1].left = j2;
+	}
+	else if (direction == 3)
+	{
+		MAP_junctionOrderArray[j1].down = j2;
+	}
+}
+
 // Adds the current square as a junction
 void MAP_addJunction()
 {
-	posY_ = MAP_currentPos[0];
-	posX_ = MAP_currentPos[1];
+	int posY_ = MAP_currentPos[0];
+	int posX_ = MAP_currentPos[1];
+
+	// Adds directions between last junction and this one
+	MAP_addJunctionDir(MAP_currentJunction, MAP_junctionCount, MAP_lastJunctionDir);
+	MAP_addJunctionDir(MAP_junctionCount, MAP_currentJunction, (MAP_currentDir - 2) % 4);
 	
 	// Increments counters
-	MAP_currentJunction++;
-	MAP_junctionCount++;
+	MAP_currentJunction = MAP_junctionCount++;
 
 	// Adds it in the map array
 	MAP_setSquareDescription(5, posY_, posX_);
-	MAP_array[posY_][posX_].junctionNumber = MAP_currentJunction;   // junction number odefinierat
-	
+	MAP_array[posY_][posX_].junctionNumber = MAP_currentJunction;
+
 	// Adds it in the junction array
 	MAP_junctionOrderArray[MAP_currentJunction].hasUnex = 1;
 	MAP_junctionOrderArray[MAP_currentJunction].posY = posY_;
@@ -206,7 +269,7 @@ int MAP_lastUnexJunction(int x)
 	if (x == 0)
 	{
 		/* Kartan är helt utforskad!!! */
-		return -1;
+		return 420;
 	}
 	else if (MAP_junctionOrderArray[x - 1].hasUnex == 1)
 	{
@@ -218,25 +281,74 @@ int MAP_lastUnexJunction(int x)
 	}
 }
 
-// FLAGz man!
-unsigned int operatingMode_; // 0 = normal, 1 = rotating, 2 = moving forward, 3 = return to prev sq, 4 = resQ
-// unsigned int rotating_;
-// unsigned int movingForward_;
-// unsigned int movingToPrevSq_;
-// unsigned int resQActive_;
+void MAP_rotate()
+{
+	// Simulation code starts here
+	MAP_currentDir = MAP_nextDir;
+	int rotating_ = 0;
+	// Simulation code ends here
+}
+
+void MAP_moveForward()
+{
+	// Simulation code starts here
+	if (MAP_currentDir == 0)
+	{
+		MAP_currentPos[1]++;
+	}
+	else if (MAP_currentDir == 1)
+	{
+		MAP_currentPos[0]--;
+	}
+	else if (MAP_currentDir == 2)
+	{
+		MAP_currentPos[1]--;
+	}
+	else if (MAP_currentDir == 3)
+	{
+		MAP_currentPos[0]++;
+	}
+	movingForward_ = 0;
+	// Simulation code ends here
+}
+
+int MAP_getDirection(int j1, int j2)
+{
+	if (MAP_junctionOrderArray[j1].right == j2)
+	{
+		return 0;
+	}
+	else if (MAP_junctionOrderArray[j1].up == j2)
+	{
+		return 1;
+	}
+	else if (MAP_junctionOrderArray[j1].left == j2)
+	{
+		return 2;
+	}
+	else if (MAP_junctionOrderArray[j1].down == j2)
+	{
+		return 3;
+	}
+}
 
 void MAP_main()
 {
-	posY_ = MAP_currentPos[0];
-	posX_ = MAP_currentPos[1];
+	int posY_ = MAP_currentPos[0];
+	int posX_ = MAP_currentPos[1];
 	
 	// Normal searching phase
-	if (!operatingMode_)
+	if (!operatingMode_ && !rotating_ && !movingForward_)
 	{
 		MAP_countUnexploredSquares();
 		// If it's visited junction
 		if (MAP_array[posY_][posX_].visited && MAP_array[posY_][posX_].description == 5)
 		{
+			// Add the distance between this and the last junction
+			MAP_addJunctionDir(MAP_currentJunction, MAP_array[posY_][posX_].junctionNumber, MAP_lastJunctionDir);
+			MAP_addJunctionDir(MAP_array[posY_][posX_].junctionNumber, MAP_currentJunction, (MAP_currentDir - 2) % 4);
+			
+
 			// Has unexplored roads
 			if (MAP_unexploredSquares)
 			{
@@ -246,8 +358,11 @@ void MAP_main()
 			else
 			{
 				MAP_junctionOrderArray[MAP_array[posY_][posX_].junctionNumber].hasUnex = 0;
+				MAP_nextDir = (MAP_currentDir - 2) % 4;
+				rotating_ = 1;
+				movingForward_ = 1;
 				operatingMode_ = 3;
-				goto mode3; // Go to previous junction
+				goto afterNormal; // Go to previous junction
 			}
 		}
 		// If it's a new square
@@ -261,39 +376,92 @@ void MAP_main()
 			// Or if it's a dead end
 			else if (MAP_unexploredSquares == 0)
 			{
-				goto mode3; // Go to previous junction
+				MAP_nextDir = (MAP_currentDir - 2) % 4;
+				rotating_ = 1;
+				movingForward_ = 1;
+				operatingMode_ = 3;
+				goto afterNormal; // Go to previous junction
 			}
 		}
 		MAP_decideDirection('a'); // Uses random
 		if (MAP_currentDir != MAP_nextDir)
 		{
-			operatingMode_ = 1;
+			rotating_ = 1;
 		}
+		movingForward_ = 1;
 	}
 	
 	// Rotating phase
-	if (operatingMode_ == 1)
+	afterNormal:
+	if (rotating_)
 	{
 		/* Rotera lämpligt - Egen fas
 				-ändra currentDir
 				-hur veta vilken mode ska följa denna???*/
+
+		// Simulation code starts here
+		MAP_rotate();
+		// Simulation code ends here
 	}
 	
 	// Moving forward phase
-	if (operatingMode_ == 2)
+	if (movingForward_ && !rotating_)
 	{
 		// åk framåt
+
+		// Simulation code starts here
+		MAP_moveForward();
+		// Simulation code ends here
 	}
 	
-	// return to prev phase
-	mode3:
-	if (operatingMode_ == 3)
+	// Go until next junction phase
+	if (operatingMode_ == 3 && !rotating_ && !movingForward_)
 	{
 		// åk tillbaka till föregående korsning
+		// ändrar direction om det behövs
+		if (MAP_checkDir((MAP_currentDir - 1) % 4))
+		{
+			MAP_nextDir = (MAP_currentDir - 1) % 4;
+			rotating_ = 1;
+		}
+		else if (MAP_checkDir((MAP_currentDir + 1) % 4))
+		{
+			MAP_nextDir = (MAP_currentDir + 1) % 4;
+			rotating_ = 1;
+		}
+		movingForward_ = 1;
+		if (MAP_currentJunction == MAP_nextJunctionShort)
+		{
+			operatingMode_ = 2;
+			movingForward_ = 0;
+		}
+	}
+
+	// Go to a junction far, far away
+	if (operatingMode_ == 2 && !rotating_ && !movingForward_)
+	{
+		MAP_decideDestination();
+		if (MAP_currentJunction == MAP_nextJunctionLong)
+		{
+			operatingMode_ = 0;
+		}
+		else if (MAP_nextJunctionLong == 420)
+		{
+			LOOPer = 0;
+		}
+		else
+		{
+			MAP_nextDir = MAP_getDirection(MAP_currentJunction, MAP_nextJunctionShort);
+			if (!(MAP_currentDir == MAP_nextDir))
+			{
+				rotating_ = 1;
+			}
+			movingForward_ = 1;
+		}
 	}
 	
 	// ResQ phase
-	if (operatingMode_ == 4)
+	if (operatingMode_ == 4 && !rotating_ && movingForward_)
 	{
 		// Go ResQ.PL, go!
 	}
@@ -306,7 +474,7 @@ TODO
 */
 int main(void)
 {
-	do {
+	/*do {
 		
 		MAP_countUnexploredSquares();
 		if (MAP_unexploredSquares >= 1)
@@ -356,7 +524,11 @@ int main(void)
 	} while (MAP_array[MAP_currentPos[0]][MAP_currentPos[1]].goal == 0)
 	
 	
-	// Påbörja undsättning - Egen fas
-		
+	// Påbörja undsättning - Egen fas*/
+
+	while (LOOPer)
+	{
+		MAP_main();
+	}
 	return 0;
 }
--- KONSTRUKTION/KOD/Map/Map/Map.h
+++ KONSTRUKTION/KOD/Map/Map/Map.h
@@ -2,14 +2,17 @@
 #define MAP_H
 
 // Variables
-int MAP_unexploredSquares = 0;
-int MAP_currentPos[2] = {16, 15}; // NOTE: (y,x) base!!!!!! NOT (x,y)!
-int MAP_currentDir = 1; // 0 = Right, 1 = Up, 2 = Left, 3 = Down 
-int MAP_goalPosition[2]; // The (y,x) pos of the goal
-int MAP_nextDir = 1; // Tells which direction we should face before moving forward again
-int MAP_currentJunction = -1; // Tells us which is the current junction (or last visited), 0-63
-int MAP_junctionCount = 0; // The number of junctions discovered so far
-int MAP_travelledDist = 0; // Number of squares travelled from the previous junction
+unsigned int MAP_unexploredSquares = 0;
+unsigned int MAP_currentPos[2] = {16, 15}; // NOTE: (y,x) base!!!!!! NOT (x,y)!
+unsigned int MAP_currentDir = 1; // 0 = Right, 1 = Up, 2 = Left, 3 = Down 
+unsigned int MAP_goalPosition[2]; // The (y,x) pos of the goal
+unsigned int MAP_nextDir = 1; // Tells which direction we should face before moving forward again
+unsigned int MAP_currentJunction = 0; // Tells us which is the current junction (or last visited), 0-63
+unsigned int MAP_junctionCount = 0; // The number of junctions discovered so far
+unsigned int MAP_travelledDist = 0; // Number of squares travelled from the previous junction
+unsigned int MAP_lastJunctionDir = 0; // Direction out of the last junction
+unsigned int MAP_nextJunctionShort = 0; // The immediate next desired junction
+unsigned int MAP_nextJunctionLong = 0; // The long-term desired junction
 
 // Structs
 struct MAP_square
@@ -17,17 +20,19 @@ struct MAP_square
 	unsigned int goal :1;
 	unsigned int visited :1;
 	unsigned int description :3;
+	unsigned int junctionNumber :6;
 };
 struct MAP_junction // Contains the distance to every connected junction
 {
-	unsigned int distRight :5;
-	unsigned int distUp :5;
-	unsigned int distLeft :5;
-	unsigned int distDown :5;
+	// The junction number in the given direction
+	unsigned int right :6;
+	unsigned int up :6;
+	unsigned int left :6;
+	unsigned int down :6;
 	
 	// eller kanske detta
 	unsigned int hasUnex :1; // Flag if unexplored roads are present
-	unsigned int poxY :4;
+	unsigned int posY :4;
 	unsigned int posX :5;
 	
 	// när man kommit till korsning med bara utforskade vägar, kolla rekursivt om korsningen innan har 
@@ -43,10 +48,11 @@ struct MAP_junction MAP_junctionOrderArray[64]; // The junction square structs. 
 
 // Methods
 void MAP_decideDirection(char);
-int MAP_checkDirUp();
-int MAP_checkDirDown();
-int MAP_checkDirLeft();
-int MAP_checkDirRight();
+int MAP_checkDirUp(int);
+int MAP_checkDirDown(int);
+int MAP_checkDirLeft(int);
+int MAP_checkDirRight(int);
+int MAP_checkDir(int);
 void MAP_setGoal();
 void MAP_setSquareDescription(int, int, int);
 void MAP_setVisited();
@@ -54,7 +60,11 @@ void MAP_countUnexploredSquares();
 void MAP_decideDestination();
 void MAP_addJunction();
 void MAP_addJunctionDist(int);
+void MAP_addJunctionDir(int, int, int);
 int MAP_lastUnexJunction();
+void MAP_rotate();
+void MAP_moveForward();
+int MAP_getDirection(int, int);
 void MAP_main();
 
 #endif
