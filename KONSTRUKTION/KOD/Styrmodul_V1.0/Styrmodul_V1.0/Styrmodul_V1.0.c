9c9c547b28049a03f2ea938faac351452e1eb237
robop <robop806@student.liu.se>
2015-05-26 11:39:21 +0200

    fixat korsningar. lite problem med slutkorsningen

--- KONSTRUKTION/KOD/Styrmodul_V1.0/Styrmodul_V1.0/Styrmodul_V1.0.c
+++ KONSTRUKTION/KOD/Styrmodul_V1.0/Styrmodul_V1.0/Styrmodul_V1.0.c
@@ -718,61 +718,58 @@ void set_map_right(int desc)
 	uint8_t posY_ = MAP_currentPos[0];
 	uint8_t posX_ = MAP_currentPos[1];
 	
-	MAP_array[posY_][posX_ + 1].description = desc;
-	
-// 	if (MAP_array[posY_][posX_ + 1].description == 0)
-// 	{
-// 		MAP_array[posY_][posX_ + 1].description = desc;
-// 	}
-// 	else if (MAP_array[posY_][posX_ + 1].description == 4 && desc == 3)
-// 	{
-// 		MAP_array[posY_][posX_ + 1].description = desc;
-// 	}
+	if (MAP_array[posY_][posX_ + 1].description == 5)
+	{
+		return;
+	}
+	else
+	{
+		MAP_array[posY_][posX_ + 1].description = desc;
+	}
 }
 
 void set_map_up(int desc)
 {
 	uint8_t posY_ = MAP_currentPos[0];
 	uint8_t posX_ = MAP_currentPos[1];
-	MAP_array[posY_ - 1][posX_].description = desc;
-// 	if (MAP_array[posY_ - 1][posX_].description == 0)
-// 	{
-// 		MAP_array[posY_ - 1][posX_].description = desc;
-// 	}
-// 	else if (MAP_array[posY_ - 1][posX_].description == 4 && desc == 3)
-// 	{
-// 		MAP_array[posY_ - 1][posX_].description = desc;
-// 	}
+	
+	if (MAP_array[posY_ - 1][posX_].description == 5)
+	{
+		return;
+	}
+	else
+	{
+		MAP_array[posY_ - 1][posX_].description = desc;
+	}
 }
 
 void set_map_left(int desc)
 {
 	uint8_t posY_ = MAP_currentPos[0];
 	uint8_t posX_ = MAP_currentPos[1];
-	MAP_array[posY_][posX_ - 1].description = desc;
-// 	if (MAP_array[posY_][posX_ - 1].description == 0)
-// 	{
-// 		MAP_array[posY_][posX_ - 1].description = desc;
-// 	}
-// 	else if (MAP_array[posY_][posX_ - 1].description == 4 && desc == 3)
-// 	{
-// 		MAP_array[posY_][posX_ - 1].description = desc;
-// 	}
+	if (MAP_array[posY_][posX_ - 1].description == 5)
+	{
+		return;
+	}
+	else
+	{
+		MAP_array[posY_][posX_ - 1].description = desc;
+	}
 }
 
 void set_map_down(int desc)
 {
 	uint8_t posY_ = MAP_currentPos[0];
 	uint8_t posX_ = MAP_currentPos[1];
-	MAP_array[posY_ + 1][posX_].description = desc;
-// 	if (MAP_array[posY_ + 1][posX_].description == 0)
-// 	{
-// 		MAP_array[posY_ + 1][posX_].description = desc;
-// 	}
-// 	else if (MAP_array[posY_ + 1][posX_].description == 4 && desc == 3)
-// 	{
-// 		MAP_array[posY_ + 1][posX_].description = desc;
-// 	}
+	
+	if (MAP_array[posY_ + 1][posX_].description == 5)
+	{
+		return;
+	}
+	else
+	{
+		MAP_array[posY_ + 1][posX_].description = desc;	
+	}
 }
 
 void set_map_FourWay()
@@ -1031,8 +1028,6 @@ char resque_mode = 'd'; /*if resque_mode == d, then the robot will search accord
 						if resque_mode == q, then the robot has found the mark and will
 						start resquing.
 						*/
-
-
 
 // P-control function
 int P_Control()
@@ -1428,11 +1423,11 @@ void TURN_Back(int mode)
 	{
 		if( offset_ > 20)
 		{
-			MOTOR_RotateLeft(180 - angle_);
+			MOTOR_RotateLeft(180); //TODO - FIXA ANGLE
 		}
 		else
 		{
-			MOTOR_RotateLeft(180 + angle_);
+			MOTOR_RotateLeft(180);
 		}
 				
 		_delay_ms(100);
@@ -1561,11 +1556,6 @@ void DISCOVERY_SetMode()
 */
 void JUNCTION_ThreeWayONE()
 {
-	MOTOR_Stop();
-	LCD_SendString("   3V3   ");
-	_delay_ms(25);
-	MOTOR_Forward(standard_speed_);
-	
 	if (discovery_mode == 'l')
 	{
 		TURN_Left(1);
@@ -1573,14 +1563,7 @@ void JUNCTION_ThreeWayONE()
 	
 	else if (discovery_mode == 'b')
 	{
-		MOTOR_Stop();
-		_delay_ms(250);
-		_delay_ms(250);
-		_delay_ms(250);
-		_delay_ms(250);
-		MOTOR_Forward(standard_speed_);
 		TURN_Back(1);
-
 	}
 	
 	else if (discovery_mode == '?')
@@ -1640,14 +1623,6 @@ void JUNCTION_ThreeWayTWO()
 	}
 	else if (discovery_mode == 'b')
 	{
-		MOTOR_Stop();
-		_delay_ms(250);
-		_delay_ms(250);
-		_delay_ms(250);
-		_delay_ms(250);
-		LCD_Clear();
-		LCD_SendString(" TUUURN BACK 3v2");
-		MOTOR_Forward(standard_speed_);
 		TURN_Back(2);
 	}
 	else if ((discovery_mode == 'l') || (discovery_mode == 'f'))
@@ -1790,20 +1765,26 @@ void MAP_main()
 			
 			//lägg till kanske: om korsningen bredvid mig har mig som granne är det okej.
 			//skapa rekursiv funktion?
+			
 			int fatIf_;
 			fatIf_ = MAP_unexploredSquares -
-					(MAP_array[posY_ - 1][posX_].description == 5 && !MAP_junctionOrderArray[MAP_array[posY_ - 1][posX_].junctionNumber].hasUnex) -
-					(MAP_array[posY_ + 1][posX_].description == 5 && !MAP_junctionOrderArray[MAP_array[posY_ + 1][posX_].junctionNumber].hasUnex) -
-					(MAP_array[posY_][posX_ - 1].description == 5 && !MAP_junctionOrderArray[MAP_array[posY_][posX_ - 1].junctionNumber].hasUnex) -
-					(MAP_array[posY_][posX_ + 1].description == 5 && !MAP_junctionOrderArray[MAP_array[posY_][posX_ + 1].junctionNumber].hasUnex);
-			if (fatIf_ >= 1)
+			(MAP_array[posY_ - 1][posX_].description == 5 && !MAP_junctionOrderArray[MAP_array[posY_ - 1][posX_].junctionNumber].hasUnex) -
+			(MAP_array[posY_ + 1][posX_].description == 5 && !MAP_junctionOrderArray[MAP_array[posY_ + 1][posX_].junctionNumber].hasUnex) -
+			(MAP_array[posY_][posX_ - 1].description == 5 && !MAP_junctionOrderArray[MAP_array[posY_][posX_ - 1].junctionNumber].hasUnex) -
+			(MAP_array[posY_][posX_ + 1].description == 5 && !MAP_junctionOrderArray[MAP_array[posY_][posX_ + 1].junctionNumber].hasUnex);
+			
+			MAP_currentJunction = MAP_array[posY_][posX_].junctionNumber;
+			
+			if (fatIf_ == 1)
 			{
-				MAP_currentJunction = MAP_array[posY_][posX_].junctionNumber;
-				//MAP_junctionOrderArray[MAP_array[posY_][posX_].junctionNumber].hasUnex = 0;
+				MAP_junctionOrderArray[MAP_currentJunction].hasUnex = 0;
 			}
+		
 			else if(fatIf_ <= 0)
 			{
-				MAP_currentJunction = MAP_array[posY_][posX_].junctionNumber;
+				MOTOR_Stop();
+				LCD_Clear();
+				LCD_SendString(" FATIF  <= 0 ");
 				MAP_junctionOrderArray[MAP_currentJunction].hasUnex = 0;
 				MAP_decideDestination();
 				MAP_operatingMode_ = 2;
@@ -1936,14 +1917,7 @@ void MAP_main()
 	// Checks if all the map has been explored
 	//MAP_checkIfDone();
 	MOTOR_Stop();
-	LCD_Clear();
-	LCD_display_uint8(MAP_array[MAP_currentPos[0]][MAP_currentPos[1]].description);
-	_delay_ms(250);
-	_delay_ms(250);
-	_delay_ms(250);
-	_delay_ms(250);	
 }
-
 
 void Floor_Marker()
 {
@@ -2334,7 +2308,7 @@ void AutomaticControl()
 		MAP_rotate();
 		angle_ = 0;
 	}
-	if((distance_counter >= 10) && (distance_flag == 1))
+	if((distance_counter >= 7) && (distance_flag == 1))
 	{
 		MAP_moveForward();
 		
@@ -2342,11 +2316,18 @@ void AutomaticControl()
 		
 		MAP_main();
 		//DISCOVERY_SetMode();
+	
+// 		if (discovery_mode == 'b')
+// 		{
+// 			LCD_Clear();
+// 			LCD_SendString(" back in corridor wtf ");
+// 			_delay_ms(250);
+// 			_delay_ms(250);
+// 			_delay_ms(250);
+// 			_delay_ms(250);
+// 			TURN_Back(4);
+// 		}
 		
-		if (discovery_mode == 'b')
-		{
-			//TURN_Back(4);
-		}
 		//MAP_rotate();
 		distance_flag = 0;
 	}
@@ -2397,20 +2378,17 @@ int main(void)
     		{
 	    		AutomaticControl();
 				Send_sensor_values();
-				LCD_Clear();
+				
 				LCD_SetPosition(0);
-				LCD_SendString("NJL:");
-				LCD_display_uint16(MAP_nextJunctionLong);
-				LCD_SendString("  ");
-				LCD_SendString("NJS");
-				LCD_display_uint16(MAP_nextJunctionShort);
-				LCD_SendString(" ");
-				LCD_SetPosition(16);
 				LCD_SendString("jC:");
 				LCD_display_uint16(MAP_junctionCount);
 				LCD_SendString(" ");
 				LCD_SendString("CJ:");
 				LCD_display_uint16(MAP_currentJunction);
+				LCD_SendString(" ");
+				LCD_SetPosition(16);
+				LCD_SendString("Unex:");
+				LCD_display_uint16(MAP_junctionOrderArray[MAP_currentJunction].hasUnex);
 				LCD_SendString(" ");
     		}
 			while(!MAP_LOOPer)
@@ -2418,7 +2396,7 @@ int main(void)
 				MOTOR_Stop();
 				LCD_Clear();
 				LCD_SetPosition(0);
-				LCD_SendString("Map Looper 0 ");
+				LCD_SendString("   I R WIN   ");
 				_delay_ms(75);
 // 				LCD_display_uint8(MAP_array[14][16].description);
 // 				LCD_SendCharacter(' ');
@@ -2460,6 +2438,9 @@ int main(void)
 				LCD_SendString(" ");
 				LCD_SendString("Re");
 				LCD_display_uint8(REFLEX_GetMarker());
+				LCD_SendString(" ");
+				LCD_SendString("F");
+				LCD_display_uint16(FRONT_SENSOR_VALUE());
 				LCD_SendString(" ");
 				_delay_ms(10);
     		}
